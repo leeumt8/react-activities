bd4716b4bc4f00a1e01ff1b855266bdf02fa3051
leeum <liam.tate@hotmail.com>
2020-02-03 19:58:47 -0500

    finished act 2

--- src/components/HelloReact.js
+++ src/components/HelloReact.js
@@ -1,7 +1,17 @@
 import React from "react";
-
+/*
 function HelloReact() {
   return <p>Hello World!</p>;
 }
+*/
 
+const HelloReact = () => {
+  return (
+    <>
+      <h1>Test h1 tag</h1>
+      <p>Hello world</p>
+    </>
+  )
+}
 export default HelloReact;
+
