58a5b296e3ffbebcf9a5061fb23e577eba6cb66a
leeum <liam.tate@hotmail.com>
2020-02-03 20:56:28 -0500

    finished activity-05

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Navbar from "./components/Navbar.js";
+import Jumbotron from "./components/Jumbotron.js";
+import Card from "./components/Card.js";
+//import {Navbar, Jumbotron, Card} from "./components"
 
 function App() {
-  return <p>Render Bootstrap components here</p>;
+  return ( <> <Navbar/>
+  <Jumbotron/>
+  <Card/>
+  </> )
 }
 
 export default App;
