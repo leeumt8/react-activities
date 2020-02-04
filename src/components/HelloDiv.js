e02355a94a2ccd6be28921a605138e8c948f45a2
leeum <liam.tate@hotmail.com>
2020-02-03 20:07:58 -0500

    finished activity 3

--- src/components/HelloDiv.js
+++ src/components/HelloDiv.js
@@ -1,5 +1,14 @@
+import React from "react";
+
 function HelloDiv() {
-  return null;
+  return <>
+    <h1>Hello! My name is Liam</h1>
+    <ul>
+      <li>soccer</li>
+      <li>vidyagames</li>
+      <li>music</li>
+    </ul>
+  </>
 }
 
 export default HelloDiv;
