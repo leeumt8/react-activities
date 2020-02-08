75206f7525c52601c4303477d60dd49ccc52d5a9
leeum <liam.tate@hotmail.com>
2020-02-08 09:49:49 -0500

    activity-19 initial

--- src/App.js
+++ src/App.js
@@ -1,35 +1,8 @@
 import React from "react";
-import FriendCard from "./components/FriendCard";
-import Wrapper from "./components/Wrapper";
-import friends from "./friends.json";
-import "./App.css";
+import Form from "./components/Form";
 
-class App extends React.Component {
-  state = {friends};
-
-  removeFriend = id => {
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    this.setState({ friends });
-  };
-
-  render() {
-    return (
-    <Wrapper>
-      <h1 className="title">Friends List</h1>
-      {this.state.friends.map(friend => (
-          <FriendCard
-            removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
-          />
-        ))}
-    </Wrapper>
-  );
-  }
+function App() {
+  return <Form />;
 }
 
 export default App;
