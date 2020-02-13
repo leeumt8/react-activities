8b6b4ce7c2ad6606443d7f1e2516c21e457c58f5
leeum <liam.tate@hotmail.com>
2020-02-12 19:46:18 -0500

    state10 initial

--- src/components/CardTitleText/index.js
+++
@@ -1,7 +0,0 @@
-import React, {useState} from "react";
-
-function CardTitleText() {
-  return <h2>Name Here</h2>;
-}
-
-export default CardTitleText;
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,37 +1,11 @@
-import axios from "axios";
-
-const languages = [
-  "JavaScript",
-  "Python",
-  "C",
-  "Ruby",
-  "Java",
-  "PHP",
-  "C#"
-];
-// Export an object containing methods we'll use for accessing the random user API
-export default {
-  getUsersByLanguage: function(language) {
-    return new Promise((resolve, reject) => {
-      axios.get("https://randomuser.me/api/?results=15").then((res) => {
-        const users = res.data.results;
-        const results = users.map((user) => {
-          return {
-            firstname: user.name.first,
-            lastname: user.name.last,
-            email: user.email,
-            image: user.picture.large,
-            language: language
-          };
-        });
-        resolve(results);
-      }).catch((err) => reject(err));
+// This code is meant to serve as a mock fetch from an API.
+export const getDeveloper = new Promise(function(resolve) {
+  setTimeout(() => {
+    resolve({
+      excitementLevel: 10000,
+      lifeLongLearner: true,
+      mood: "excited",
+      name: "Alec"
     });
-  },
-  // Return a Promise to simulate an async call
-  getLanguagesList: function() {
-    return new Promise((resolve) => {
-      resolve(languages);
-    });
-  }
-};
+  }, 1000);
+});
