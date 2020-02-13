c896738754ab423ea612b3da48dcd5e44c3d935d
leeum <liam.tate@hotmail.com>
2020-02-12 20:59:12 -0500

    state10 finished

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -4,18 +4,22 @@ import SearchForm from "../../components/SearchForm";
 import SearchResults from "../../components/SearchResults";
 import Alert from "../../components/Alert";
 import API from "../../utils/API";
+import ArticleContext from "../../context/ArticleContext";
 
 function Search() {
   const [search, setSearch] = useState("Wikipedia");
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [url, setUrl] = useState("");
   const [error, setError] = useState("");
+
+  const [articleState, setArticleState] = useState({
+    title: "",
+    description: "",
+    url: ""
+  });
 
   // When the component mounts, update the title to be Wikipedia Searcher
   useEffect(() => {
     document.title = "Wikipedia Searcher";
-
+    console.log(search);
     if (!search) {
       return;
     }
@@ -28,11 +32,16 @@ function Search() {
         if (res.data.status === "error") {
           throw new Error(res.data.message);
         }
-        setTitle(res.data[1]);
+        setArticleState({
+          title: res.data[1],
+          description: res.data[2][0],
+          url: res.data[3][0]
+        });
+        /* setTitle(res.data[1]);
         setDescription(res.data[2][0]);
-        setUrl(res.data[3][0]);
+        setUrl(res.data[3][0]); */
       })
-      .catch(err => setError(err));
+      .catch(err => setArticleState(err));
   }, [search]);
 
   const handleInputChange = event => {
@@ -43,7 +52,7 @@ function Search() {
     event.preventDefault();
   };
   return (
-    <div>
+    <ArticleContext.Provider value={articleState}>
       <Container style={{ minHeight: "100vh" }}>
         <h1 className="text-center">Search For Anything on Wikipedia</h1>
         <Alert type="danger" style={{ opacity: error ? 1 : 0, marginBottom: 10 }}>
@@ -54,9 +63,9 @@ function Search() {
           handleInputChange={handleInputChange}
           results={search}
         />
-        <SearchResults title={title} description={description} url={url} />
+        <SearchResults title={articleState.title} description={articleState.description} url={articleState.url} />
       </Container>
-    </div>
+    </ArticleContext.Provider>
   );
 }
 
