0c208bbaab8721ac6a9a8b9db28a73555eade973
leeum <liam.tate@hotmail.com>
2020-02-15 10:23:59 -0500

    state17 finished

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useReducer } from "react";
 
 function Count() {
   const dogs = [
@@ -11,6 +11,27 @@ function Count() {
       image: "https://images.dog.ceo/breeds/husky/n02110185_1511.jpg"
     }
   ];
+  /* const [count, dispatch] = useReducer((state,action) => {
+    if(action === "praise") {
+      return state + 1
+    }
+  }, 0); */
+
+  function reducer(state, action) {
+    switch (action) {
+      case "goodHarry":
+        return {...state, HarryGoods: state.HarryGoods + 1}
+      case "goodHermione":
+        return {...state, HermioneGoods: state.HermioneGoods + 1}
+      default:
+        throw new Error()
+    }
+  }
+
+  const [state,dispatch] = useReducer(reducer, {
+    HarryGoods: 0,
+    HermioneGoods: 0
+  })
 
   return (
     <div className="App">
@@ -20,8 +41,8 @@ function Count() {
             <img className="card-img-top" src={item.image} alt={item.name} />
             <div className="card-body">
               <h4 className="card-title">{item.name}</h4>
-              <p className="card-text">{item.name} has been praised __ times!</p>
-              <button className="btn btn-primary">Praise</button>
+              <p className="card-text">{item.name} has been praised {state[item.name + "Goods"]} times!</p>
+              <button className="btn btn-primary" onClick={() => dispatch(`good${item.name}`)}>Praise</button>
             </div>
           </div>
         ))}
