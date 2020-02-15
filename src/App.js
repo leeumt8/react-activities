24321b8a5a65d82072422ad14c8c3575a9404150
leeum <liam.tate@hotmail.com>
2020-02-15 09:29:14 -0500

    state17 initial

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React from "react";
-import Gallery from "./pages/Gallery";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Wrapper from "./components/Wrapper";
 
-function App() {
+function Count() {
+  const dogs = [
+    {
+      name: "Harry",
+      image: "https://images.dog.ceo/breeds/vizsla/n02100583_10960.jpg"
+    },
+    {
+      name: "Hermione",
+      image: "https://images.dog.ceo/breeds/husky/n02110185_1511.jpg"
+    }
+  ];
+
   return (
-    <div>
-      <Navbar />
-      <Wrapper>
-        <Gallery />
-      </Wrapper>
-      <Footer />
+    <div className="App">
+      <div className="row mt-5">
+        {dogs.map(item => (
+          <div key={item.name} className="card mx-auto col-4">
+            <img className="card-img-top" src={item.image} alt={item.name} />
+            <div className="card-body">
+              <h4 className="card-title">{item.name}</h4>
+              <p className="card-text">{item.name} has been praised __ times!</p>
+              <button className="btn btn-primary">Praise</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Count;
